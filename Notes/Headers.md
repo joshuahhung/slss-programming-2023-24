c2db773caa68a1a09687ecd33f63ce36924b24ef
Joshua Hung <joshuahung501@gmail.com>
2023-09-19 15:02:27 -0700

    Added notes to NLP and added Python file

---
+++ Notes/Headers.md
@@ -0,0 +1,11 @@
+At the top of our Python files, we're going to put in 3 comments 
+
+> # Title
+> # Author
+> # Date
+
+```python
+# Input and Output
+# Author: Josh
+# Date: 19 Septemeber 2023
+```
---
+++ Notes/input_and_output.py
@@ -0,0 +1,11 @@
+# Input and Output
+# Author: Josh
+# Date: 10 Septemeber 2023
+
+# Output a quote from someone famous
+print("Git is Google Drive on caffeine. --Ubial, probably")
+
+# Add two more quotes
+print("meow --cat, probably")
+
+print("woof --dog, probably")
