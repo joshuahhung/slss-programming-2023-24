519267a2f43828b175423a757f976b5bd2a234ae
Joshua Hung <joshuahung501@gmail.com>
2023-10-11 13:30:15 -0700

    added pyramid, coutndown, and new year

--- # loop practice.py
+++ # loop practice.py
@@ -2,30 +2,47 @@
 # author: Josh
 # date: 2023-10-10
 
-import time
+# Create a list of groceries
+groceries = ["hot wheels", "lego", "ice cream", "video games", "carrots"]
 
-# create a list of groceries
-groceries = ["hot wheels", "lego", "ice cream", "video games"]
-
-# do something for each thing in the list
-# print it out in a pretty way
+# Do something for each thing in the list
+# Print it out in a pretty way
 # e.g.
-#  * hotwheels
-#    ---
-#  * lego
-#    ---
-# etc.
+#     * hot wheels
+#       ---
+#     * lego
+#       ---
+#       etc.
+
+# print(f"* {groceries[0]}")
+# print(f"  ---")
+# print(f"* {groceries[1]}")
+# print(f"  ---")
+# print(f"* {groceries[2]}")
+# print(f"  ---")
 
 for item in groceries:
-    print(f"# {item}\n  ---")
+    print(f"* {item}")
+    print(f"  ---")
 
-# print pyramid thing
-for i in range(6):
-    print("*" * (i + 1))
+# Using the above method, create the thing below:
+# *
+# **
+# ***
+# ****
+# *****
+# ******
 
-# new year countdown
-for i in range(10, 0, -1):
-    print(i)
+pyramid = ["*", "**", "***", "****", "*****", "******"]
+
+for row in pyramid:
+    print(f"* {row}")
+
+import time
+
+countdown = ["10", "9", "8", "7", "6", "5", "4", "3", "2", "1"]
+for number in countdown:
+    print(number)
     time.sleep(1)
 
-print("HAPPY NEW YEAR")
+print("Happy New Year!")
