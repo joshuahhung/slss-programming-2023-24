d49578d375f0275b821a6ef5feebc1b4cfe95a32
Joshua Hung <joshuahung501@gmail.com>
2023-10-25 13:28:14 -0700

    deleted Code possibilities

--- food_suggestions_bot.py
+++ food_suggestions_bot.py
@@ -10,3 +10,10 @@
 # Introduce the bot to the user
 # Ask the user what their favourite food is
 # 
+
+words = ["tickle", "pop", "roll", "dance"]
+output = ""
+for word in words:
+  output = output + word[1]
+print(output)
+
