4b70fd9370e9689cb90c389f2d611757059316f1
Joshua Hung <joshuahung501@gmail.com>
2023-10-30 08:48:14 -0700

    Added Bubble Tea Counter

---
+++ fave_bubble_tea.py
@@ -0,0 +1,40 @@
+# Bubble Tea Popularity Algorithm
+# Author: Ubial
+# Date: 27 October 2023
+
+# Ask 5 users what their favourite bubble tea place is
+# prints out a summary of the data
+
+# ------ CONSTANTS
+
+NUM_RESPONDENTS = 5
+
+
+coco_likes = 0 # Initialize the variable to 0
+suntea_likes = 0 
+chatime_likes = 0
+bubqueen_likes = 0
+
+
+for _ in range(NUM_RESPONDENTS):
+
+    # Ask the user what their favourite place is
+    print("what's your favourite bubble tea place? ")
+
+    fave_place = input( ).strip(",.?!").lower()
+
+    # Tally or counting algo
+    if fave_place == "coco":
+        coco_likes = coco_likes + 1
+    elif fave_place == "suntea":
+        suntea_likes += 1
+    elif fave_place == "chatime":
+        chatime_likes += 1
+    elif fave_place == "bubble queen":
+        bubqueen_likes += 1
+
+# Repeat the code above 5 times
+
+# Print out a summary of all places
+# Give the raw score AND the percentage
+print(f"CoCo Likes: {coco_likes}")
