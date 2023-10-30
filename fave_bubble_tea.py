021a1dff6cc6604e1f5f0cbdea53c8a74ae3368c
Joshua Hung <joshuahung501@gmail.com>
2023-10-30 09:14:58 -0700

    added other summary and percentages round function

--- fave_bubble_tea.py
+++ fave_bubble_tea.py
@@ -7,13 +7,14 @@
 
 # ------ CONSTANTS
 
-NUM_RESPONDENTS = 5
+NUM_RESPONDENTS = 7
 
 
 coco_likes = 0 # Initialize the variable to 0
 suntea_likes = 0 
 chatime_likes = 0
 bubqueen_likes = 0
+other_likes = 0
 
 
 for _ in range(NUM_RESPONDENTS):
@@ -32,24 +33,33 @@ for _ in range(NUM_RESPONDENTS):
         chatime_likes += 1
     elif fave_place == "bubble queen":
         bubqueen_likes += 1
+    elif fave_place == "other":
+        other_likes += 1
 
 # Repeat the code above 5 times
 
 # Print out a summary of all places
 # Give the raw score AND the percentage
 
+
+y = round(3.1415, 2)
+
 # Summary of Coco
 print(f"CoCo Likes: {coco_likes}")
-print(coco_likes/NUM_RESPONDENTS * 100)
+print(round(coco_likes/NUM_RESPONDENTS * 100, 2))
 
 # Summary of Suntea
 print(f"Suntea Likes: {suntea_likes}")
-print(suntea_likes/NUM_RESPONDENTS * 100)
+print(round(suntea_likes/NUM_RESPONDENTS * 100, 2))
 
 # Summary of Chatime
 print(f"Chatime Likes: {chatime_likes}")
-print(chatime_likes/NUM_RESPONDENTS * 100)
+print(round(chatime_likes/NUM_RESPONDENTS * 100, 2))
 
 # Summary of Bubqueen
 print(f"Bubqueen Likes: {bubqueen_likes}")
-print(bubqueen_likes/NUM_RESPONDENTS * 100)
+print(round(bubqueen_likes/NUM_RESPONDENTS * 100, 2))
+
+# Summary of Other
+print(f"Other Likes: {other_likes}")
+print(round(other_likes/NUM_RESPONDENTS * 100, 2))
