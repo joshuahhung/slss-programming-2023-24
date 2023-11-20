0ac6105f763f29d1006b31950fbc3b87bd4de242
Joshua Hung <joshuahung501@gmail.com>
2023-11-20 13:52:52 -0800

    Added parental bot

---
+++ parental_bot.py
@@ -0,0 +1,26 @@
+# Parental Bot
+# Author: Josh
+# Nov. 17, 2023
+
+# Ask first question
+print("Did you eat?")
+kid = input().strip(" ").lower().split(" ")
+
+# Ask second question
+print("Did you study?")
+kid = input().strip(" ").lower().split(" ")
+
+# Ask third question
+print("Did you do your laundry?")
+kid = input().strip(" ").lower().split(" ")
+
+# Ask fourth question
+print("Did you call grandma?")
+kid = input().strip(" ").lower().split(" ")
+
+# Initialize our mom score
+mom_score = 0
+
+with open("./data.csv") as f:
+    # Throw away the header line
+    header = f.readline()
